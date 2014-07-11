95eb0a9c5067863365cc82fb01065af6382460a3
Spencer Barnes-Israel <s.barnes.israel@gmail.com>
2014-07-11 12:02:27 -0400

    Finished frequency.py for Problem 4

--- assignment1/frequency.py
+++ assignment1/frequency.py
@@ -23,16 +23,21 @@ def slice_n_dice(tweet):
 	  split_list.append(split_tweet[i])
   return split_list
 
-def print_dict(dict_to_print):
-  for i in range(len(dict_to_print)):
-    print dict_to_print[i] dict_to_print
+def calc_freq(term_count, sum):
+  freq = 0
+  term_count = float(term_count)
+  freq = term_count / sum
+  return freq
 
 def main():
     tweet_file = open(sys.argv[1])
 
     tweets = []
-    tweet_counts = {}
+    tweet_wordcounts = {}
+    tweet_wordfreqs = {}
+    tweet_sum = 0
 
+    # Take each line in the tweet dump and grab just the tweet itself, no metadata.
     for line in tweet_file:
       try:
         tweet_lines = json.loads(line, encoding='utf-8')
@@ -42,14 +47,27 @@ def main():
         # Do nothing, because we don't care about those non-Unicode tweets for now.
         pass
 
+    # Chop each word in the tweet and compute the sum of the words
     for i in range(len(tweets)-1):
+    # Get counts for each word
       sliced_tweet = slice_n_dice(tweets[i])
       for j in range(len(sliced_tweet)-1):
-        if sliced_tweet[j] in tweet_counts:
-          tweet_counts[sliced_tweet[j]] += 1
+        if sliced_tweet[j] in tweet_wordcounts:
+          tweet_wordcounts[sliced_tweet[j]] += 1
         else:
-          tweet_counts[sliced_tweet[j]] = 1
-    print tweet_counts
+          tweet_wordcounts[sliced_tweet[j]] = 1
+    # Compute sum of each word's count
+    tweet_sum = sum(tweet_wordcounts.itervalues())
+
+    # Compute the frequency of each word
+    for key in tweet_wordcounts:
+      tweet_wordfreqs[key] = calc_freq(tweet_wordcounts[key], tweet_sum)
+    for k,v in tweet_wordfreqs.items():
+      try:
+        print "%s %f" % (k, v)
+      except UnicodeEncodeError:
+        # Not sure how to correct this error
+        pass
     
 
 if __name__ == '__main__':
